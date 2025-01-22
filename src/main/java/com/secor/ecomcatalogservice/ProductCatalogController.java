95d058ef05154f50acb1a42f63d6dd0f98891638
jose-kurian <josekurian87@gmail.com>
2025-01-23 00:41:04 +0530

    Code refactoring

--- src/main/java/com/secor/ecomcatalogservice/ProductCatalogController.java
+++ src/main/java/com/secor/ecomcatalogservice/ProductCatalogController.java
@@ -1,6 +1,8 @@
 package com.secor.ecomcatalogservice;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,26 +12,32 @@ import java.util.List;
 @RequestMapping("/products")
 public class ProductCatalogController {
 
+    private final Logger LOG = LoggerFactory.getLogger(ProductCatalogController.class);
+
     @Autowired
     private ProductCatalogRepository productCatalogRepository;
 
     @GetMapping
     public List<ProductCatalog> getAllProducts() {
+        LOG.info("getAllProducts");
         return productCatalogRepository.findAll();
     }
 
     @GetMapping("/{productId}")
     public ProductCatalog getInventoryForProductId(@PathVariable Long productId) {
+        LOG.info("getInventoryForProductId({})", productId);
         return productCatalogRepository.findByProductId(productId);
     }
 
     @PostMapping
     public ProductCatalog addProduct(@RequestBody ProductCatalog product) {
+        LOG.info("addProduct({})", product);
         return productCatalogRepository.save(product);
     }
 
     @PutMapping("/{id}")
     public ProductCatalog updateProduct(@PathVariable Long id, @RequestBody ProductCatalog productDetails) {
+        LOG.info("updateProduct({})", productDetails);
         ProductCatalog product = productCatalogRepository.findById(id).orElseThrow();
         product.setProductName(productDetails.getProductName());
         product.setDescription(productDetails.getDescription());
@@ -40,6 +48,7 @@ public class ProductCatalogController {
 
     @DeleteMapping("/{id}")
     public void deleteProduct(@PathVariable Long id) {
+        LOG.info("deleteProduct({})", id);
         productCatalogRepository.deleteById(id);
     }
 }
